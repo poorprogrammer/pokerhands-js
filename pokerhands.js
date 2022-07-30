023072c34d7612eb5340f76e249ff30e7c6558d9
Phongsak Ritpitakphong <jane.makub@gmail.com>
2022-07-30 22:41:55 +0700

    Remove data clump and move responsiblity

--- pokerhands.js
+++ pokerhands.js
@@ -7,10 +7,17 @@ class PokerGame {
     isPairGame() {
         return this.hand1.hasPair() || this.hand2.hasPair();
     }
+
+    player1WinWithOnePair() {
+        return this.hand1.pairRank > this.hand2.pairRank;
+    }
+
+    player1WinWithHighCard() {
+        return this.hand1.highestRank > this.hand2.highestRank;
+    }
 }
 
 class PokerHand {
-
     constructor(fiveCardsInput) {
         this.cards = this.sortByCardRanks(fiveCardsInput)
     }
@@ -46,24 +53,13 @@ const player1Win = (input) => {
     if (!input) {
         return false;
     }
-    let hand1 = new PokerHand(input.substring(0, 14));
-    let hand2 = new PokerHand(input.substring(15, 29));
-    let game = new PokerGame(hand1, hand2)
+    let game = new PokerGame(new PokerHand(input.substring(0, 14)), new PokerHand(input.substring(15, 29)));
 
     if (game.isPairGame()) {
-        return player1WinWithOnePair(game)
+        return game.player1WinWithOnePair()
     }
-    return player1WinWithHighCard(hand1, hand2);
+    return game.player1WinWithHighCard();
 };
-
-const player1WinWithOnePair = (game) => {
-    return game.hand1.pairRank > game.hand2.pairRank;
-}
-
-const player1WinWithHighCard = (hand1, hand2) => {
-    return hand1.highestRank > hand2.highestRank;
-}
-
 
 module.exports = {
     player1Win
