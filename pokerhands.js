089b636a76dc5a77e08dddf0b90cb6dd2efeee25
Phongsak Ritpitakphong <jane.makub@gmail.com>
2022-07-30 21:09:25 +0700

    win with high card

--- pokerhands.js
+++ pokerhands.js
@@ -1,6 +1,59 @@
-const player1Win = () => {
+const player1Win = (input) => {
+    if (!input) {
+        return false;
+    }
 
+    if (player1WinWithHighCard(input)) {
+        return true;
+    }
+    // } else if (player1WinWithOnePair(input)) {
+    //     return true;
+    // }
+    return false;
+    // return player1WinWithOnePair(input) || player1WinWithHighCard(input);
 };
+
+
+const player1WinWithHighCard = (cardsInput) => {
+    hand1 = new PokerHand(cardsInput.substring(0, 14));
+    hand2 = new PokerHand(cardsInput.substring(15, 29));
+    return hand1.highestRank > hand2.highestRank;
+}
+
+const player1WinWithOnePair = (cardsInput) => {
+    hand1 = new PokerHand(cardsInput.substring(0, 14));
+    hand2 = new PokerHand(cardsInput.substring(15, 29));
+    return hand1.has1Pair();
+}
+
+class PokerHand {
+    constructor(fiveCardsInput) {
+        this.cards = fiveCardsInput;
+    }
+
+    get highestRank() {
+        return this.sortCardRanks()[0];
+    };
+
+    has1Pair() {
+        return true;
+    }
+
+    get pairRank() {
+        return 1;
+    }
+
+    sortCardRanks() {
+        let cardRanks = [];
+        this.cards.split(" ").forEach(card => {
+            cardRanks.push("23456789TJQKA".indexOf(card[0]))
+        });
+        return cardRanks.sort(function (a, b) {
+            return b - a;
+        });
+    }
+}
+
 module.exports = {
     player1Win
 }
--- pokerhands.spec.js
+++ pokerhands.spec.js
@@ -1,5 +1,24 @@
 const poker = require("./pokerhands");
 
-it('should work', () => {
-	expect(true).toEqual(true);
+it('should return false when have no game', () => {
+	expect(poker.player1Win("")).toEqual(false);
+});
+
+it('should got player 1 win with highest card', () => {
+	expect(poker.player1Win("AC 3C 4C 8C 6C 2H 3H 4H 5H 7H")).toEqual(true);
+	expect(poker.player1Win("AH 2C 4C 8C 6C 2H 3H 4H 5H 7H")).toEqual(true);
+	expect(poker.player1Win("2C AH 4C 8C 6C 2H 3H 4H 5H 7H")).toEqual(true);
+	expect(poker.player1Win("2C 4C AH 8C 6C 2H 3H 4H 5H 7H")).toEqual(true);
+});
+
+it('should got player 1 lose with highest card', () => {
+	expect(poker.player1Win("2H 3C 4C 8C 6C AH 3H 4H 5H 7H")).toEqual(false);
+});
+
+xit('should got player 1 win with only 1 pair', () => {
+	expect(poker.player1Win("2H 2C 4C 8C 6C AH 3H 4H 5H 7H")).toEqual(true);
+});
+
+xit('should got player 1 lose with only 1 pair', () => {
+	expect(poker.player1Win("AH 3C 4C 8C 6C 3H 3H 4H 5H 7H")).toEqual(false);
 });
