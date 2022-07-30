5632ab5e9004cfe33540287995dcb5e29c28f5c9
Phongsak Ritpitakphong <jane.makub@gmail.com>
2022-07-30 22:24:34 +0700

    remove data clump and add win with one pair logic

--- pokerhands.js
+++ pokerhands.js
@@ -1,51 +1,39 @@
-const player1Win = (input) => {
-    if (!input) {
-        return false;
+class PokerGame {
+    constructor(hand1, hand2) {
+        this.hand1 = hand1;
+        this.hand2 = hand2;
     }
 
-    if (player1WinWithHighCard(input)) {
-        return true;
+    isPairGame() {
+        return this.hand1.hasPair() || this.hand2.hasPair();
     }
-    // } else if (player1WinWithOnePair(input)) {
-    //     return true;
-    // }
-    return false;
-    // return player1WinWithOnePair(input) || player1WinWithHighCard(input);
-};
-
-
-const player1WinWithHighCard = (cardsInput) => {
-    hand1 = new PokerHand(cardsInput.substring(0, 14));
-    hand2 = new PokerHand(cardsInput.substring(15, 29));
-    return hand1.highestRank > hand2.highestRank;
-}
-
-const player1WinWithOnePair = (cardsInput) => {
-    hand1 = new PokerHand(cardsInput.substring(0, 14));
-    hand2 = new PokerHand(cardsInput.substring(15, 29));
-    return hand1.has1Pair();
 }
 
 class PokerHand {
+
     constructor(fiveCardsInput) {
-        this.cards = fiveCardsInput;
+        this.cards = this.sortByCardRanks(fiveCardsInput)
     }
 
     get highestRank() {
-        return this.sortCardRanks()[0];
+        return this.cards[0];
     };
 
-    has1Pair() {
-        return true;
-    }
-
     get pairRank() {
-        return 1;
+        if (this.hasPair()) {
+            return this.pairCardRank[0];
+        }
+        return -1;
     }
 
-    sortCardRanks() {
+    hasPair() {
+        this.pairCardRank = this.cards.filter((card, index) => index !== this.cards.indexOf(card))
+        return this.pairCardRank.length === 1;
+    }
+
+    sortByCardRanks(cards) {
         let cardRanks = [];
-        this.cards.split(" ").forEach(card => {
+        cards.split(" ").forEach(card => {
             cardRanks.push("23456789TJQKA".indexOf(card[0]))
         });
         return cardRanks.sort(function (a, b) {
@@ -53,6 +41,29 @@ class PokerHand {
         });
     }
 }
+
+const player1Win = (input) => {
+    if (!input) {
+        return false;
+    }
+    let hand1 = new PokerHand(input.substring(0, 14));
+    let hand2 = new PokerHand(input.substring(15, 29));
+    let game = new PokerGame(hand1, hand2)
+
+    if (game.isPairGame()) {
+        return player1WinWithOnePair(game)
+    }
+    return player1WinWithHighCard(hand1, hand2);
+};
+
+const player1WinWithOnePair = (game) => {
+    return game.hand1.pairRank > game.hand2.pairRank;
+}
+
+const player1WinWithHighCard = (hand1, hand2) => {
+    return hand1.highestRank > hand2.highestRank;
+}
+
 
 module.exports = {
     player1Win
--- pokerhands.spec.js
+++ pokerhands.spec.js
@@ -15,10 +15,15 @@ it('should got player 1 lose with highest card', () => {
 	expect(poker.player1Win("2H 3C 4C 8C 6C AH 3H 4H 5H 7H")).toEqual(false);
 });
 
-xit('should got player 1 win with only 1 pair', () => {
+it('should got player 1 win with only 1 pair', () => {
 	expect(poker.player1Win("2H 2C 4C 8C 6C AH 3H 4H 5H 7H")).toEqual(true);
 });
 
-xit('should got player 1 lose with only 1 pair', () => {
-	expect(poker.player1Win("AH 3C 4C 8C 6C 3H 3H 4H 5H 7H")).toEqual(false);
+it('should got player 1 lose with only 1 pair', () => {
+	expect(poker.player1Win("AH 3C 4C 8C 6C 3H 3S 4H 5H 7H")).toEqual(false);
+});
+
+it('should got player 1 lose with higher rank pair ', () => {
+	expect(poker.player1Win("3H 3S 4C 8C 6C AH AC 4H 5H 7H")).toEqual(false);
+	expect(poker.player1Win("4C KH 8C 6C KC JH JS 4H 5H 7H")).toEqual(true);
 });
